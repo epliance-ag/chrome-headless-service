3ab34d218a2239f1806d3df0ead8052f04b0832b
Laurenz Gamper <laurenzgamper@gmail.com>
2018-02-21 14:12:20 +0100

    fix typo

--- test/diffpdf.sh
+++ test/diffpdf.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-diffpdf reference/simplepage.pdf resutl/simplepage.pdf
-diffpdf reference/simplepagelandscape.pdf resutl/simplepagelandscape.pdf
+diffpdf reference/simplepage.pdf result/simplepage.pdf
+diffpdf reference/simplepagelandscape.pdf result/simplepagelandscape.pdf
 diffpdf reference/hugetable.pdf result/hugetable.pdf
 diffpdf reference/hugetext.pdf result/hugetext.pdf
 
