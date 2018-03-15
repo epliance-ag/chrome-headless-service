e1d0dbaa398f0109f1867bb0e721c7faccb74a9d
Laurenz Gamper <laurenzgamper@gmail.com>
2018-03-15 10:07:55 +0100

    show chrome version on startup

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/dumb-init /bin/sh
+/usr/bin/google-chrome-stable --version
 /usr/bin/google-chrome-stable --no-sandbox --disable-gpu --headless --hide-scrollbars --disable-dev-shm-usage \
 --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --user-data-dir=/data &  # launch chrome in the background
 
