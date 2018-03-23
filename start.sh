c8f4037bfc00dbbeb06dcc8282affb844f4a91f6
Laurenz Gamper <laurenzgamper@gmail.com>
2018-03-23 10:38:55 +0100

    disable some features

--- start.sh
+++ start.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/dumb-init /bin/sh
 /usr/bin/google-chrome-stable --version
-/usr/bin/google-chrome-stable --no-sandbox --disable-gpu --headless --hide-scrollbars --disable-dev-shm-usage \
+/usr/bin/google-chrome-stable --no-sandbox --disable-gpu --bwsi --disable-3d-apis --disable-cloud-import --disable-databases \
+            --disable-es3-apis --disable-es3-gl-context --disable-extensions --disable-local-storage --disable-reading-from-canvas \
+              --disable-remote-fonts --headless --hide-scrollbars --disable-dev-shm-usage \
 --remote-debugging-address=0.0.0.0 --remote-debugging-port=9222 --user-data-dir=/data &  # launch chrome in the background
 
 npm start
